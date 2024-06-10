99158ff861aa10d1cb68808904b4b67305abcac1
Ko Shin <koshin.1116@gmail.com>
2024-06-10 17:31:15 +0800

    feat: decode mp3 to wav

--- composeApp/src/androidMain/kotlin/com/github/kkoshin/muse/export/ExportScreen.kt
+++ composeApp/src/androidMain/kotlin/com/github/kkoshin/muse/export/ExportScreen.kt
@@ -2,6 +2,9 @@
 
 package com.github.kkoshin.muse.export
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,16 +13,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toUri
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.transformer.Effects
+import com.github.foodiestudio.sugar.ExperimentalSugarApi
+import com.github.foodiestudio.sugar.storage.AppFileHelper
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -32,12 +42,17 @@ data class ExportArgs(
  * 2. play the exported audio file
  */
 @OptIn(UnstableApi::class)
+@kotlin.OptIn(ExperimentalSugarApi::class)
 @Composable
 fun ExportScreen(
     modifier: Modifier = Modifier,
     args: ExportArgs,
+    viewModel: ExportViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
     val context = LocalContext.current
+    val appFileHelper = remember {
+        AppFileHelper(context.applicationContext)
+    }
     val videoExportPipeline =
         rememberVideoExportPipeline(
             context = context,
@@ -48,11 +63,23 @@ fun ExportScreen(
             ),
         )
 
-    val audiExportPipeline =
+    val audioExportPipeline =
         rememberAudioExportPipeline(
             context = context,
-            input = args.audioUri.toUri(),
+//            input = args.audioUri.toUri(),
+            input = appFileHelper.requireFilesDir(false).resolve("decoded.wav").toUri(),
         )
+
+    var selectedMp3: Uri? by remember {
+        mutableStateOf(null)
+    }
+    val mp3FilePicker =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.OpenDocument()) {
+            if (it != null) {
+                selectedMp3 = it
+                viewModel.testDecodeMp3(selectedMp3!!)
+            }
+        }
 
     Scaffold(
         modifier = modifier,
@@ -73,10 +100,25 @@ fun ExportScreen(
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    ExportButton(modifier = Modifier, exportPipeline = audiExportPipeline)
+                    Button(onClick = {
+                        if (selectedMp3 != null) {
+                            viewModel.testDecodeMp3(selectedMp3!!)
+                        } else {
+                            mp3FilePicker.launch(arrayOf("audio/*"))
+                        }
+                    }) {
+                        Text(text = "Decode Mp3 to WAV")
+                    }
+                    AppendSilenceToWavButton()
+                    ExportButton(modifier = Modifier, exportPipeline = audioExportPipeline)
                     ExportButton(modifier = Modifier, exportPipeline = videoExportPipeline)
                 }
             }
         },
     )
+}
+
+@Composable
+fun AppendSilenceToWavButton() {
+//    TODO("Not yet implemented")
 }
