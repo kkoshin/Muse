ed60979e4060f5715df9ea6c89e259334435059f
Ko Shin <koshin.1116@gmail.com>
2024-06-11 18:53:16 +0800

    feat: update UI

--- composeApp/src/androidMain/kotlin/com/github/kkoshin/muse/editor/EditorScreen.kt
+++ composeApp/src/androidMain/kotlin/com/github/kkoshin/muse/editor/EditorScreen.kt
@@ -6,26 +6,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.github.kkoshin.muse.export.ExportButton
+import com.github.kkoshin.muse.export.rememberAudioExportPipeline
 import kotlinx.serialization.Serializable
 import org.koin.androidx.compose.koinViewModel
+import kotlin.time.Duration.Companion.seconds
 
 @Serializable
 data class EditorArgs(
@@ -67,7 +73,7 @@ fun EditorScreen(
                         Button(onClick = {
                             viewModel.startTTS(args.phrases.map { it.lowercase() })
                         }) {
-                            Text(text = "Start")
+                            Text(text = "Start to TTS")
                         }
                     }
 
@@ -76,15 +82,45 @@ fun EditorScreen(
                             Row(verticalAlignment = Alignment.CenterVertically) {
                                 CircularProgressIndicator(Modifier.padding(end = 12.dp))
                                 Column {
-                                    Text(text = "Processing ${value}%")
-                                    Text(text = "Processing phrase: $phrase")
+                                    // TODO(Jiangc): 展示具体的进度
+                                    Text(text = "Processing...")
+//                                    Text(text = "Processing phrase: $phrase")
                                 }
                             }
                         }
                     }
 
                     is ProgressStatus.Success -> {
-                        Text(text = "Completed: ${(progress as ProgressStatus.Success).audios}")
+                        var silence by remember {
+                            mutableFloatStateOf(1.0f)
+                        }
+                        val context = LocalContext.current
+                        val audioExportPipeline =
+                            rememberAudioExportPipeline(
+                                context = context,
+                                input = (progress as ProgressStatus.Success).pcm,
+                                paddingSilence = silence.toInt().seconds,
+                            )
+
+                        LaunchedEffect(silence) {
+                            audioExportPipeline.paddingSilence = silence.toInt().seconds
+                        }
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            Text(
+                                text = "Silence: ${silence.toInt()} second(s)",
+                                modifier = Modifier.fillMaxWidth(),
+                            )
+                            Slider(value = silence, valueRange = 0f..5.0f, onValueChange = {
+                                silence = it
+                            })
+                            ExportButton(modifier = Modifier, exportPipeline = audioExportPipeline)
+                        }
                     }
 
                     is ProgressStatus.Failed -> {
@@ -97,18 +133,6 @@ fun EditorScreen(
                             }
                         }
                     }
-                }
-            }
-        },
-        floatingActionButton = {
-            if (progress is ProgressStatus.Success) {
-                FloatingActionButton(onClick = {
-                    onExport(
-                        (progress as ProgressStatus.Success).pcm,
-                        (progress as ProgressStatus.Success).audios,
-                    )
-                }) {
-                    Icon(Icons.Filled.ArrowForward, contentDescription = null)
                 }
             }
         },
