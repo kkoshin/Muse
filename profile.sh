91caf73917b56dfaf99a3dae7b0158689d3ee89f
Ko Shin <koshin.1116@gmail.com>
2024-07-12 11:33:57 +0800

    chore: update profile.sh

--- profile.sh
+++ profile.sh
@@ -7,7 +7,7 @@
 # brew install gradle-profiler
 
 ### set JAVA_HOME target JAVA 17
-export JAVA_HOME="/Users/ray/Library/Java/JavaVirtualMachines/jbr-17.0.7/Contents/Home"
+#export JAVA_HOME="/Users/ray/Library/Java/JavaVirtualMachines/jbr-17.0.7/Contents/Home"
 ### clean
 ./gradlew clean
 ### Run
