cc6cb5b3652b1199c1b1d2f7efc1a3cd2fa3d2b0
Ko Shin <koshin.1116@gmail.com>
2024-10-20 16:02:28 +0800

    refactor: using BillingHelper

--- composeApp/src/androidMain/kotlin/io/github/kkoshin/muse/payment/SubscriptionManager.kt
+++ composeApp/src/androidMain/kotlin/io/github/kkoshin/muse/payment/SubscriptionManager.kt
@@ -2,24 +2,9 @@ package io.github.kkoshin.muse.payment
 
 import android.app.Activity
 import android.content.Context
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.BillingClient.BillingResponseCode
-import com.android.billingclient.api.BillingClient.FeatureType
 import com.android.billingclient.api.BillingClient.ProductType
-import com.android.billingclient.api.BillingClientStateListener
-import com.android.billingclient.api.BillingFlowParams
-import com.android.billingclient.api.BillingResult
 import com.android.billingclient.api.ProductDetails
 import com.android.billingclient.api.Purchase
-import com.android.billingclient.api.QueryProductDetailsParams
-import com.android.billingclient.api.QueryPurchasesParams
-import com.android.billingclient.api.queryProductDetails
-import com.android.billingclient.api.queryPurchasesAsync
-import kotlinx.collections.immutable.persistentListOf
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
 
 /**
  * Subscriptions automatically renew until they are canceled. A subscription can go through the following states:
@@ -35,122 +20,25 @@ import kotlin.coroutines.suspendCoroutine
  */
 class SubscriptionManager(context: Context) {
 
-    private val billingClient = BillingClient.newBuilder(context)
-        .setListener { billingResult, purchases ->
-            // 展示最终的支付结果,这里仅限刚刚完成支付的时候，还可以延后查询支付状态
-            when (billingResult.responseCode) {
-                BillingResponseCode.OK -> {
-                    if (!purchases.isNullOrEmpty()) {
-                        purchases.forEach {
-                            // 支付成功后，得到此次支付的唯一凭证：purchaseToken
-                            it.purchaseToken
-                        }
-                    }
-                }
-                BillingResponseCode.USER_CANCELED -> {
-                    // user cancelled
-                }
-                else -> {
-                    // other error code
-                }
-            }
-        }
-        .build()
-
-    // 调用任何接口之前需要确保已经连接上
-    private suspend fun BillingClient.requireConnectionSetup(): Boolean =
-        suspendCoroutine { continuation ->
-            if (isReady) {
-                continuation.resume(true)
-                return@suspendCoroutine
-            }
-            startConnection(object : BillingClientStateListener {
-                override fun onBillingServiceDisconnected() {
-                    continuation.resume(false)
-                }
-
-                override fun onBillingSetupFinished(billingResult: BillingResult) {
-                    continuation.resume(billingResult.responseCode == BillingResponseCode.OK)
-                }
-            })
-        }
-
-    private suspend inline fun withConnection(block: BillingClient.() -> Unit) {
-        if (billingClient.requireConnectionSetup()) {
-            block(billingClient)
-        }
-    }
+    private val helper = BillingHelper(context)
 
     // 检查当前设备所安装的 GMS 是否支持订阅，避免版本过低
-    fun checkGooglePlayServiceAvailable(): Boolean {
-        return listOf(FeatureType.SUBSCRIPTIONS, FeatureType.SUBSCRIPTIONS_UPDATE)
-            .all {
-                billingClient.isFeatureSupported(it).responseCode == BillingResponseCode.OK
-            }
-    }
+    fun checkGooglePlayServiceAvailable(): Boolean = helper.checkSubscriptionsSupported()
 
-    suspend fun queryProductDetails(productId: String): ProductDetails? {
-        withConnection {
-            val queryProductDetailsParams = QueryProductDetailsParams.newBuilder()
-                .setProductList(
-                    persistentListOf(
-                        QueryProductDetailsParams.Product.newBuilder()
-                            .setProductId(productId)
-                            .setProductType(ProductType.SUBS)
-                            .build()
-                    )
-                ).build()
-            val productDetailsResult = withContext(Dispatchers.IO) {
-                queryProductDetails(queryProductDetailsParams)
-            }
-            if (productDetailsResult.billingResult.responseCode == BillingResponseCode.OK) {
-                return productDetailsResult.productDetailsList?.get(0)
-            }
-        }
-        return null
-    }
+    suspend fun queryProductDetails(productId: String): Result<ProductDetails?> =
+        helper.queryProductDetails(productId, ProductType.SUBS).map { it.firstOrNull() }
 
     /**
      * 最终结果是
      */
-    suspend fun purchase(activity: Activity, productDetails: ProductDetails) {
-        withConnection {
-            val productDetailsParamsListBuilder =
-                BillingFlowParams.ProductDetailsParams.newBuilder()
-                    .setProductDetails(productDetails)
-            productDetails.subscriptionOfferDetails?.firstOrNull()?.offerToken?.let {
-                productDetailsParamsListBuilder.setOfferToken(it)
-            }
-
-            val billingFlow = BillingFlowParams.newBuilder()
-                .setProductDetailsParamsList(
-                    persistentListOf(
-                        productDetailsParamsListBuilder.build()
-                    )
-                )
-                .build()
-            launchBillingFlow(activity, billingFlow)
-        }
+    suspend fun launchPurchase(
+        activity: Activity,
+        productDetails: ProductDetails,
+        onResult: (Result<Purchase>) -> Unit
+    ) = helper.purchase(activity, productDetails) {
+        onResult(it.map { list -> list.firstOrNull()!! })
     }
 
     // 对于在非App内进行购买，可以主动进行对当前用户的订阅进行查询
-    suspend fun queryPurchases(): Result<List<Purchase>> {
-        withConnection {
-            val param = QueryPurchasesParams.newBuilder()
-                .setProductType(ProductType.SUBS)
-                .build()
-            queryPurchasesAsync(param).let {
-                return when (it.billingResult.responseCode) {
-                    BillingResponseCode.OK -> {
-                        Result.success(it.purchasesList)
-                    }
-
-                    else -> {
-                        Result.failure(Exception("queryPurchases failed: ${it.billingResult.debugMessage}"))
-                    }
-                }
-            }
-        }
-        return Result.failure(Exception("connection failed"))
-    }
+    suspend fun queryPurchases(): Result<List<Purchase>> = helper.queryPurchases(ProductType.SUBS)
 }
