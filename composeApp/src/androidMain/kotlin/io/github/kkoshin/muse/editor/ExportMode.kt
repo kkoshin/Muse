69d1f6b3db330c977cd23a8dd8876e370b406016
Ko Shin <koshin.1116@gmail.com>
2025-07-13 22:08:48 +0800

    fix: crash on launch

--- composeApp/src/androidMain/kotlin/io/github/kkoshin/muse/editor/ExportMode.kt
+++ composeApp/src/androidMain/kotlin/io/github/kkoshin/muse/editor/ExportMode.kt
@@ -1,8 +1,14 @@
 package io.github.kkoshin.muse.editor
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class ExportMode {
     // 朗读模式
+    @SerialName("reading")
     Reading,
     // 听写模式
+    @SerialName("dictation")
     Dictation,
 }
