92ac6fc1ac192de2717fa8afc24d930f6b81d13d
Ko Shin <koshin.1116@gmail.com>
2025-07-13 22:38:41 +0800

    refactor: route arguments

--- composeApp/src/androidMain/kotlin/io/github/kkoshin/muse/editor/ExportMode.kt
+++ composeApp/src/androidMain/kotlin/io/github/kkoshin/muse/editor/ExportMode.kt
@@ -1,14 +1,15 @@
 package io.github.kkoshin.muse.editor
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
 enum class ExportMode {
     // 朗读模式
-    @SerialName("reading")
     Reading,
     // 听写模式
-    @SerialName("dictation")
-    Dictation,
+    Dictation;
+
+    companion object {
+        fun fromName(name: String): ExportMode? {
+            return entries.find { it.name == name }
+        }
+    }
+
 }
