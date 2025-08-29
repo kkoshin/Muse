a87f22252c7291e6c8da0411b5d34a473f0c3905
Ko Shin <koshin.1116@gmail.com>
2025-08-29 15:27:11 +0800

    feat: support edge to edge

--- composeApp/src/androidMain/kotlin/io/github/kkoshin/muse/AppTheme.kt
+++ composeApp/src/androidMain/kotlin/io/github/kkoshin/muse/AppTheme.kt
@@ -1,7 +1,6 @@
 package io.github.kkoshin.muse
 
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
@@ -11,24 +10,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.graphics.Color
 
-private val lightScrim = android.graphics.Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
-private val darkScrim = android.graphics.Color.argb(0x80, 0x1b, 0x1b, 0x1b)
-
 @Composable
 fun AppTheme(activity: ComponentActivity, content: @Composable () -> Unit) {
     val darkTheme = isSystemInDarkTheme()
 
     DisposableEffect(darkTheme) {
-        activity.enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.auto(
-                android.graphics.Color.TRANSPARENT,
-                android.graphics.Color.TRANSPARENT,
-            ) { darkTheme },
-            navigationBarStyle = SystemBarStyle.auto(
-                lightScrim,
-                darkScrim,
-            ) { darkTheme },
-        )
+        activity.enableEdgeToEdge()
         onDispose {}
     }
 
